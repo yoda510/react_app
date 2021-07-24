49c7b22fc4f25b79939f5a1514de6ddc05503207
Nathan Beach <yoda510@gmail.com>
2021-07-24 15:12:47 -0500

    Error Fixes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Layout from './hocs/Layout';
 import Home from './components/Home';
-import MarsData from './components/MarsData';
 import PictureDay from './components/PictureDay';
 
 
@@ -12,7 +11,7 @@ const App = () => (
             <Switch>
                 <Route exact path='/' component={Home} />
                 <Route exact path='/picture' component={PictureDay} />
-                <Route exact path='/rover' component={MarsData} />
+        
             </Switch>
         </Layout>
     </Router>
